dc8b31c257f4e3afca4fd612da4200edfe634081
tiago-mends <tiagomendesdev@gmail.com>
2024-02-02 22:48:35 -0300

    feat: ensure semantic tags are used on footer and header

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,10 +1,12 @@
 const Footer = () => {
   return (
-    <div className="w-full bg-secondary px-5 py-6">
-      <p className="text-xs font-bold text-gray-400 opacity-75">
-        &copy; 2023 FSW Barber
-      </p>
-    </div>
+    <footer>
+      <div className="w-full bg-secondary px-5 py-6">
+        <p className="text-xs font-bold text-gray-400 opacity-75">
+          &copy; 2023 FSW Barber
+        </p>
+      </div>
+    </footer>
   );
 };
 
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -11,22 +11,24 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <Card>
-      <CardContent className="flex flex-row items-center justify-between p-5">
-        <Link href="/">
-          <Image src="/logo.png" alt="FSW Barber" width={120} height={18} />
-        </Link>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon" className="h-8 w-8">
-              <MenuIcon size={16} />
-            </Button>
-          </SheetTrigger>
+    <header>
+      <Card>
+        <CardContent className="flex flex-row items-center justify-between p-5">
+          <Link href="/">
+            <Image src="/logo.png" alt="FSW Barber" width={120} height={18} />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="h-8 w-8">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
 
-          <SideMenu />
-        </Sheet>
-      </CardContent>
-    </Card>
+            <SideMenu />
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 };
 
